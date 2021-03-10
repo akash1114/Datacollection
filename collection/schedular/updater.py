751fca2187cf80a3554eb77466c5d1dd20bd100d
akash1114 <68108181+akash1114@users.noreply.github.com>
2021-03-10 11:55:18 +0530

    Update updater.py

--- collection/schedular/updater.py
+++ collection/schedular/updater.py
@@ -5,7 +5,7 @@ from collection import update_data,dividend_update
 def start():
     scheduler = BackgroundScheduler()
     print("Starting....")
-    scheduler.add_job(update_data.data_save, 'interval', hours=1)
-    scheduler.add_job(dividend_update.dividend, 'interval', hours=1)
+    scheduler.add_job(update_data.data_save, 'interval', minutes=3)
+    scheduler.add_job(dividend_update.dividend, 'interval', minutes=3)
     scheduler.start()
     print(scheduler.print_jobs())
